598a588c2d35d04ff081c35832e6eb35cf2d0506
Rion Hutsell <st.gish@gmail.com>
2020-09-03 08:50:29 -0600

    Add specs for models

---
+++ spec/models/invoice_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe Invoice, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of :status }
+  end
+
+  describe 'relationships' do
+    it { should belong_to :merchant }
+    it { should belong_to :customer }
+    it { should have_many :invoice_items }
+    it { should have_many :transactions }
+  end
+end
